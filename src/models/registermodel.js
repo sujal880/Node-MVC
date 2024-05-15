0c68de96e5dee21ebaede0c82da9b11f39aae276
sujal880 <sujaldave880@gmail.com>
2024-05-15 13:27:34 +0530

    Some Changes

--- src/models/registermodel.js
+++ src/models/registermodel.js
@@ -1,14 +1,7 @@
 const mongoose=require('mongoose');
-const bcrypt=require('bcryptjs');
 const userSchema=new mongoose.Schema({
-    
     email:String,
     password:String
 });
-
-userSchema.pre('save',async function(next){
-    if(!this.isModified('password'))return next();
-    this.password=await bcrypt.hash(this.password,8);
-})
 const User=mongoose.model('users',userSchema);
 module.exports=User;
